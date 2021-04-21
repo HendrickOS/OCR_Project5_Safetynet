c0f5654f500c8132ddfeaca3ad1409b64fc1b015
hendrick osseux <osseux.hendrick@gmail.com>
2021-04-22 01:05:46 +0200

    Ajout URL phoneAlerts

--- src/main/java/com/safety/safetynet/controller/RequestController.java
+++ src/main/java/com/safety/safetynet/controller/RequestController.java
@@ -1,15 +1,19 @@
 package com.safety.safetynet.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.safety.safetynet.model.Person;
 import com.safety.safetynet.service.FirestationService;
 import com.safety.safetynet.service.MedicalRecordService;
 import com.safety.safetynet.service.PersonService;
 
 @RestController
-@RequestMapping
 public class RequestController {
 
 	@Autowired
@@ -19,4 +23,36 @@ public class RequestController {
 	@Autowired
 	private MedicalRecordService medicalRecordService;
 
+	@GetMapping("/childAlert")
+	public List<Person> childsFromAdress(@RequestParam String address) {
+		return null;
+	}
+
+	@GetMapping("/phoneAlert")
+	public List<String> getPhonefromPerson(@RequestParam int firestation) {
+		String station = String.valueOf(firestation);
+		List<Person> persons = firestationService.getPersonsForFirestation(station);
+		return persons.stream().map(p -> p.getPhone()).collect(Collectors.toList());
+	}
+
+	@GetMapping("/fire")
+	public List<Person> getPersonsFromAddressAndStationNumber(@RequestParam String address) {
+		return null;
+	}
+
+	@GetMapping("/stations")
+	public List<String> getPersonsSortbyAddressFromStationNumbers(@RequestParam List<Integer> stationNumbers) {
+		return null;
+	}
+
+	@GetMapping("/personInfo")
+	public List<String> getPersonAndMedicalRecordFromFirstNameAndLastName(@RequestParam String firstName,
+			String lastName) {
+		return null;
+	}
+
+	@GetMapping("/communityEmail")
+	public List<String> getEmailofAllPersonsFromCity(@RequestParam String city) {
+		return null;
+	}
 }
