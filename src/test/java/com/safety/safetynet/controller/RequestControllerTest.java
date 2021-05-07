e5568689338b9c21be8127b1d9d0ba3bb54469ce
hendrick osseux <osseux.hendrick@gmail.com>
2021-05-07 20:43:55 +0200

    ADD RequestControllerTest

---
+++ src/test/java/com/safety/safetynet/controller/RequestControllerTest.java
@@ -0,0 +1,14 @@
+package com.safety.safetynet.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RequestControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
