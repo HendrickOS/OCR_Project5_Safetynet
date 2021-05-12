796f534e4349eaed55145d9044a967766757632e
hendrick osseux <osseux.hendrick@gmail.com>
2021-05-12 20:03:33 +0200

    MAJ MedicalRecordRepositoryTest : Clear all Method except add & delete

--- src/test/java/com/safety/repository/MedicalRecordRepositoryTest.java
+++ src/test/java/com/safety/repository/MedicalRecordRepositoryTest.java
@@ -82,39 +82,73 @@ class MedicalRecordRepositoryTest {
 		assertEquals(medicalRecord.getAllergies(), result.get(0).getAllergies());
 		assertEquals(medicalRecord.getMedications(), result.get(0).getMedications());
 	}
-//
-//	@Test
-//	void updateMedicalRecordTest() {
-//		MedicalRecord result = new MedicalRecord();
-//		List<String> medications = new ArrayList<String>();
-//		medications.add("aznol:350mg");
-//		medications.add("hydrapermazol:100mg");
-//		List<String> allergies = new ArrayList<String>();
-//		allergies.add("nillacilan");
-//		MedicalRecord medicalRecord = new MedicalRecord("John", "Boyd", "03/06/1984", medications, allergies);
-//
-////		 result = medicalRecordService.updateMedicalRecord(medicalRecord);
-//		result.update(medicalRecord);
-//
-//		assertEquals(medicalRecord, result);
-//	}
-//
-////	@Test
-////	void getPersonAgeTest() {
-////		int age;
-////		Person john = new Person("John", "Boyd", "1509 Culver St", "Culver", "97451", "841-874-6512",
-////				"jaboyd@email.com");
-////		List<String> medications = new ArrayList<String>();
-////		medications.add("aznol:350mg");
-////		medications.add("hydrapermazol:100mg");
-////		List<String> allergies = new ArrayList<String>();
-////		allergies.add("nillacilan");
-////		MedicalRecord mr = new MedicalRecord("John", "Boyd", "03/06/1984", medications, allergies);
-////
-////		Mockito.when(medicalRecordService.getPersonAge(john)).thenReturn(37);
-//////		age = medicalRecordService.getPersonAge(john);
-////
-////		assertEquals(37, age);
-////	}
+
+	@Test
+	void updateMedicalRecordTest() {
+		MedicalRecord result = new MedicalRecord();
+		List<String> medications = new ArrayList<String>();
+		medications.add("aznol:350mg");
+		medications.add("hydrapermazol:100mg");
+		List<String> allergies = new ArrayList<String>();
+		allergies.add("nillacilan");
+		MedicalRecord medicalRecord = new MedicalRecord("John", "Boyd", "03/06/1984", medications, allergies);
+
+//		result = medicalRecordRepository.updateMedicalRecord(medicalRecord);
+		result.update(medicalRecord);
+		medicalRecordRepository.updateMedicalRecord(medicalRecord);
+//		result.update(medicalRecordRepository.updateMedicalRecord(medicalRecord);
+
+		assertEquals(medicalRecord.getAllergies(), result.getAllergies());
+		assertEquals(medicalRecord.getMedications(), result.getMedications());
+	}
+
+	@Test
+	void getPersonAgeTest() {
+		int age;
+		Person john = new Person("John", "Boyd", "1509 Culver St", "Culver", "97451", "841-874-6512",
+				"jaboyd@email.com");
+		List<String> medications = new ArrayList<String>();
+		medications.add("aznol:350mg");
+		medications.add("hydrapermazol:100mg");
+		List<String> allergies = new ArrayList<String>();
+		allergies.add("nillacilan");
+		MedicalRecord mr = new MedicalRecord("John", "Boyd", "03/06/1984", medications, allergies);
+
+		age = medicalRecordRepository.getPersonAge(john);
+
+		assertEquals(37, age);
+	}
+
+	@Test
+	void deleteMedicalRecordTest() {
+		List<String> medicationsJohn = new ArrayList<String>();
+		medicationsJohn.add("aznol:350mg");
+		medicationsJohn.add("hydrapermazol:100mg");
+		List<String> allergiesJohn = new ArrayList<String>();
+		allergiesJohn.add("nillacilan");
+		MedicalRecord johnMR = new MedicalRecord("John", "Boyd", "03/06/1984", medicationsJohn, allergiesJohn);
+		List<String> medicationsJacob = new ArrayList<String>();
+		medicationsJacob.add("pharmacol:5000mg");
+		medicationsJacob.add("terazine:10mg");
+		medicationsJacob.add("noznazol:250mg");
+		List<String> allergiesJacob = new ArrayList<String>();
+		MedicalRecord jacobMR = new MedicalRecord("Jacob", "Boyd", "03/06/1989", medicationsJacob, allergiesJacob);
+		List<String> medicationsTenley = new ArrayList<String>();
+		List<String> allergiesTenley = new ArrayList<String>();
+		allergiesJohn.add("peanut");
+		MedicalRecord tenleyMR = new MedicalRecord("Tenley", "Boyd", "02//18/2012", medicationsTenley, allergiesTenley);
+		List<MedicalRecord> medicalRecords = new ArrayList<MedicalRecord>();
+		medicalRecords.add(johnMR);
+		medicalRecords.add(jacobMR);
+		medicalRecords.add(tenleyMR);
+		List<MedicalRecord> result = new ArrayList<MedicalRecord>();
+		result.add(jacobMR);
+		result.add(tenleyMR);
+
+		medicalRecords.remove(jacobMR);
+		medicalRecordRepository.deleteMedicalRecord(jacobMR);
+
+//		assertEquals(result, medicalRecords);
+	}
 
 }
